e38bcf0f39555a5ca457d1d60dc5cb1e5b252b41
floge07 <floge.fg@gmail.com>
2025-01-07 17:17:43 +0100

    Add test to demonstrate issue #268

--- test/GraphQL.Conventions.Tests/Execution/SchemaExecutionTests.cs
+++ test/GraphQL.Conventions.Tests/Execution/SchemaExecutionTests.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using GraphQL.Conventions;
 using GraphQL.NewtonsoftJson;
 using Tests.Templates;
 using Tests.Templates.Extensions;
@@ -28,6 +30,73 @@ namespace Tests.Execution
         private class QueryTypeWithDecimal
         {
             public decimal Test => 10;
+        }
+
+        [Test]
+        public async Task Test_NonNull_Validation()
+        {
+            string query = @"
+                query {
+                    test {
+                        children {
+                            field(problematic: [""1"", ""2""])
+                        }
+                    }
+                }
+            ";
+
+            var engine = GraphQLEngine.New();
+
+            engine.WithQuery<NonNullTests.QueryType>();
+
+            var executor = engine
+                .NewExecutor()
+                .WithQueryString(query);
+
+            var result1 = await executor.ExecuteAsync();
+            System.Diagnostics.Debug.WriteLine(new GraphQLSerializer(indent: true).Serialize(result1));
+
+
+            var schema = Schema<NonNullTests>();
+            schema.ShouldHaveQueries(1);
+            schema.ShouldHaveMutations(0);
+            schema.Query.ShouldHaveFieldWithName("test");
+
+            var result2 = await schema.ExecuteAsync((e) => e.Query = query);
+            System.Diagnostics.Debug.WriteLine(result2);
+            ResultHelpers.AssertNoErrorsInResult(result2);
+        }
+
+        public class NonNullTests
+        {
+            public QueryType Query { get; }
+
+            public class QueryType
+            {
+                public NonNull<DataType> test()
+                {
+                    return new DataType()
+                    {
+                        children = new List<NonNull<DataType>> {
+                        new DataType(),
+                        new DataType()
+                    }
+                    };
+                }
+
+                public class DataType
+                {
+                    public NonNull<List<NonNull<DataType>>> children { get; set; }
+
+                    public List<NonNull<string>> field(NonNull<List<string>> problematic)
+                    {
+                        return new List<NonNull<string>> {
+                            new NonNull<string>("ab"),
+                            new NonNull<string>("cd")
+                        };
+                    }
+                }
+            }
         }
     }
 }
